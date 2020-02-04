fd251b637033ac777a6e82eedf22986d8019ef7a
Johnson Kow <jkow95@gmail.com>
2020-02-04 20:45:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,9 +15,10 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+     
+programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -63,6 +67,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+
 
 	programmer_hash = 
  		{
@@ -79,8 +84,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+programmer_hash= Hash.new(:yukihiro_matsumoto) 
+  
+}
 end
 
 def changing_alan
